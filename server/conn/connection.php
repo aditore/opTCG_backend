57a407517e664cfe29d79c7a2a0d8324d6a6138d
aditore <anthonyditore@gmail.com>
2022-12-09 19:39:18 -0800

    playing with the server

--- server/conn/connection.php
+++ server/conn/connection.php
@@ -5,11 +5,11 @@ require dirname(__DIR__, 2) . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
 $dotenv->safeLoad();
   # needs connection to env still (in process)  
-    echo 'My username is ' . $_ENV['USER'] . '!';
+    echo '<h1> My username is ' . $_ENV['USER'] . '! </h1>';
 
 ##MYSQL INFO NEXT
 
 $conn = new mysqli($_ENV['SERVER'], $_ENV['USER'], $_ENV['PASSWORD'], $_ENV['DB']);
 
-echo "Logged in!"
+echo '<h2> Logged in! </h2>';
 ?>
--- server/index.php
+++ server/index.php
@@ -1,3 +1,13 @@
 <?php
- echo 'making progress!!'
+    #CONNECTED TO MYSQL
+    require './conn/connection.php';
+    
+    
+    $test = 'This is a test string';
+    echo '<section>' . $test . '</section>';
+
+
+    if ($_SERVER['REQUEST_URI'] === '/index.php') {
+        echo '<p> Not on /, please go back to / to view website from the index. </p>';
+    }
 ?>
